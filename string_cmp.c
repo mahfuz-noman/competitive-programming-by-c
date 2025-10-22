b0385af790a446623b9507a248ae2d2ed75d8828
Mahfuz Ahmed Noman <mahfuz.ahmed.nomann@gmail.com>
2025-10-22 18:21:27 +0600

    Implement string comparison in string_cmp.c

---
+++ string_cmp.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+    char a[21],b[21];
+    scanf("%s %s",a,b);
+    for(int i = 0 ; ; i++){
+        if(a[i] == '\0'){
+            printf("%s",a);
+            break;
+        }
+        else if(b[i] == '\0'){
+            printf("%s",b);
+            break;
+        }
+        else if(a[i] == '\0' && b[i] == '\0'){
+            printf("%s",a);
+            break;
+        }
+        else if(a[i] > b[i]){
+            printf("%s",b);
+            break;
+        }
+        else if(a[i] < b[i]){
+            printf("%s",a);
+            break;
+        }
+    }
+    return 0;
+}
